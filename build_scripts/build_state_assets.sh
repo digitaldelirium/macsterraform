4b146b55d43afbea68d5e5465cc552f74b9000f0
Ian Cornett <ian.cornett@outlook.com>
2018-01-15 01:27:43 -0800

    add access_key for state config

--- build_scripts/build_state_assets.sh
+++ build_scripts/build_state_assets.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 CLIENT_SECRET=$1
+ACCESS_KEY=$2
 
 cd ../state
 
-terraform init
+terraform init -backend-config="access_key=$ACCESS_KEY"
 terraform plan -out state.plan
 terraform apply "state.plan"
