238ccd99f420ef33ba91635be53ba36a51bd406d
Ian Cornett <ian.cornett@outlook.com>
2018-01-15 02:09:31 -0800

    try msi login

--- build_scripts/build_service_assets.sh
+++ build_scripts/build_service_assets.sh
@@ -25,7 +25,7 @@ chmod 400 macsvm
 
 echo $SSH_PUBLIC_KEY > macsvm.pub
 
-az login --service-principal -u 44c4e2a1-4b32-4d7b-b063-ab00907ab449 -p $CLIENT_SECRET --tenant ce30a824-b64b-4702-b3e8-8ff93ba9da38
+az login --msi
 
 terraform init -backend-config="access_key=$ACCESS_KEY"
 terraform plan -out "service.plan" -var "access_key=$ACCESS_KEY" -var "pfx_password=$PFX_PASSWORD" -var "server_pk_password=$SERVER_PK_PASSWORD"
