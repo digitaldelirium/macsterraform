5c4fa421911a10708ad8ca9d81ac86b301911155
Ian Cornett <ian.cornett@outlook.com>
2018-01-17 15:59:26 -0800

    add multiple logins

--- build_scripts/build_state_assets.sh
+++ build_scripts/build_state_assets.sh
@@ -7,5 +7,9 @@ TENANT_ID=$4
 az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET  --tenant $TENANT_ID
 
 terraform init -backend-config="access_key=$ACCESS_KEY"
+
+az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET  --tenant $TENANT_ID
 terraform plan -out state.plan -var "access_key=$ACCESS_KEY"
+
+az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET  --tenant $TENANT_ID
 terraform apply "state.plan"
