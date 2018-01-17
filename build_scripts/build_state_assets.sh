1ff17308228929317be7b7c4b19b7de272770273
Ian Cornett <ian.cornett@outlook.com>
2018-01-17 15:53:04 -0800

    fix plan parameters

--- build_scripts/build_state_assets.sh
+++ build_scripts/build_state_assets.sh
@@ -7,5 +7,5 @@ TENANT_ID=$4
 az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET  --tenant $TENANT_ID
 
 terraform init -backend-config="access_key=$ACCESS_KEY"
-terraform plan -out state.plan
+terraform plan -out state.plan -var "access_key=$ACCESS_KEY"
 terraform apply "state.plan"
