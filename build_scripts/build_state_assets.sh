5f3c1e52b7443ffe6743f1e1428687921ae39c05
Ian Cornett <ian.cornett@outlook.com>
2018-01-17 15:40:35 -0800

    add parameters to build_state script

--- build_scripts/build_state_assets.sh
+++ build_scripts/build_state_assets.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 CLIENT_SECRET=$1
 ACCESS_KEY=$2
+CLIENT_ID=$3
+TENANT_ID=$4
 
-az login --msi
+az login --service-principal -u $CLIENT_ID --password $CLIENT_SECRET  --tenant $TENANT_ID
 
 terraform init -backend-config="access_key=$ACCESS_KEY"
 terraform plan -out state.plan
